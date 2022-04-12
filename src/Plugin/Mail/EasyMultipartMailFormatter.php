9e1d0ce4f6159a2d20a708cb79d292b476ae2cc9
Aaron Klump <sourcecode@intheloftstudios.com>
2022-04-12 15:36:58 -0700

    Rewrite how message is converted to multipart.

--- src/Plugin/Mail/EasyMultipartMailFormatter.php
+++ src/Plugin/Mail/EasyMultipartMailFormatter.php
@@ -36,42 +36,7 @@ class EasyMultipartMailFormatter extends HtmlMailSystem {
    *   The formatted $message, ready for sending.
    */
   public function format(array $message) {
-    $is_normalized = isset($message['body']['#type']) && 'ez_multipart_mail' === $message['body']['#type'];
-    if (!$is_normalized) {
-      $unformatted = $message;
-      $formatted = parent::format($message);
-
-      // We will only create a multipart message if the formatted version is not
-      // already multipart, because it's possible that the formatted version has
-      // already created a multipart message, depending on the settings and
-      // plugins that may or may not be present on this drupal install.
-      $formatted_type = $this->getContentType($formatted);
-
-      // "The Content-Type field for multipart entities requires one parameter,
-      // "boundary", which is used to specify the encapsulation boundary."
-      // @link https://www.w3.org/Protocols/rfc1341/7_2_Multipart.html#z0
-      $is_multipart = strstr($formatted_type, 'boundary=') !== FALSE;
-      if ($is_multipart) {
-        return $message;
-      }
-
-      // This comes from \Drupal\Core\Mail\Plugin\Mail\PhpMail::format.
-      if (is_array($unformatted['body'])) {
-        $eol = $this->siteSettings->get('mail_line_endings', PHP_EOL);
-        $unformatted['body'] = implode("$eol$eol", $unformatted['body']);
-      }
-
-      $unformatted_type = $this->getContentType($unformatted);
-      if (strstr($unformatted_type, 'text/plain') !== FALSE) {
-        $unformatted['body'] = MailFormatHelper::htmlToText($unformatted['body']);
-      }
-
-      $message['body'] = [
-        '#type' => 'ez_multipart_mail',
-        '#plain' => Markup::create($unformatted['body']),
-        '#html' => Markup::create($formatted['body']),
-      ];
-    }
+    $message = $this->ensureMessageIsMultipart($message);
 
     // This step applies all the pre/post processing to our render element.
     // Rather than use #children, we are going to cherry-pick #plain and #html
@@ -108,9 +73,9 @@ class EasyMultipartMailFormatter extends HtmlMailSystem {
    *   A message array per the drupal mail API.  Expecting the "headers" key.
    *
    * @return string
-   *   The content type if found cast to lowercase.
+   *   The content type header value if found, cast to lowercase.
    */
-  private function getContentType(array $message): string {
+  private function getContentTypeHeaderValue(array $message): string {
     foreach ($message['headers'] ?? [] as $key => $value) {
       if (strtolower($key) === 'content-type') {
         return strtolower($value);
@@ -120,4 +85,72 @@ class EasyMultipartMailFormatter extends HtmlMailSystem {
     return '';
   }
 
+  /**
+   * Convert any message to multipart text + html.
+   *
+   * @param array $message
+   *   The message render array per Drupal Mail API.
+   *
+   * @return array
+   *   The message in multipart mime.
+   */
+  private function ensureMessageIsMultipart(array $message): array {
+    if ('ez_multipart_mail' === ($message['body']['#type'] ?? NULL)) {
+      return $message;
+    }
+
+    // We will send to the parent formatting method for several reasons:
+    //  - Ensure the "from" header.
+    //  - Leverage the Mail Mime module if installed and configured OR...
+    //  - ... run the message through "theme_html_mail*".
+    //  - Force to plaintext if user account has disabled HTML mails.
+    //  - Apply configured post filters per HTML mail module.
+    $formatted_message = parent::format($message);
+
+    // We will only create a multipart message if the formatted version is not
+    // already multipart, because it's possible that the formatted version has
+    // already created a multipart message, depending on the settings and
+    // plugins that may or may not be present on this Drupal install.
+    $content_type_header = $this->getContentTypeHeaderValue($formatted_message);
+
+    // "The Content-Type field for multipart entities requires one parameter,
+    // "boundary", which is used to specify the encapsulation boundary."
+    // @link https://www.w3.org/Protocols/rfc1341/7_2_Multipart.html#z0
+    $is_multipart = strstr($content_type_header, 'boundary=') !== FALSE;
+    if ($is_multipart) {
+      return $formatted_message;
+    }
+
+    // Pull the HTML version from the parent process.
+    $html_body = $formatted_message['body'];
+    unset($formatted_message);
+
+    // At this point we have still need to create a multipart message, therefor
+    // we need to ensure we have the plaintext and the HTML parts and then
+    // assemble in an ez_multipart_mail #type render array.
+
+    // This comes from \Drupal\Core\Mail\Plugin\Mail\PhpMail::format.
+    if (is_array($message['body'])) {
+      $eol = $this->siteSettings->get('mail_line_endings', PHP_EOL);
+      $message['body'] = implode("$eol$eol", $message['body']);
+    }
+
+    // Convert the content if it's not already plaintext.
+    $content_type_header = $this->getContentTypeHeaderValue($message);
+    $is_plaintext = boolval(strstr($content_type_header, 'text/plain'));
+    if (!$is_plaintext) {
+      $message['body'] = MailFormatHelper::htmlToText($message['body']);
+    }
+
+    $message['body'] = MailFormatHelper::wrapMail($message['body']);
+
+    $message['body'] = [
+      '#type' => 'ez_multipart_mail',
+      '#plain' => Markup::create($message['body']),
+      '#html' => Markup::create($html_body),
+    ];
+
+    return $message;
+  }
+  
 }
